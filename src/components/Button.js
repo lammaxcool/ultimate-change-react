d7d894646372d6a1e9fc2a808c5db4255435a81a
Maxym <66222425+lammaxcool@users.noreply.github.com>
2021-06-02 14:35:21 +0300

    fix button

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,7 +2,9 @@ import './Button.scss';
 
 function Button(props) {
     return (
-        <button type="button" className="btn btn-primary">{props.name}</button>
+        <button type="button" className={`btn btn-primary btn-color-${props.color}`}>
+            {props.name}
+        </button>
     );
 }
 
